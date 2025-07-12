583467ad254f9465980464f9b8b1a2ae3213289b
Smah-amen <samahamen20001@gmail.com>
2025-07-12 15:45:16 +0300

    refactor responsive canvas handling and camera setup in Three.js

--- src/script.js
+++ src/script.js
@@ -1,28 +1,39 @@
-import * as THREE from 'three';
-
+import * as THREE from "three";
 
 const scene = new THREE.Scene();
 
 const canvas = document.querySelector("canvas.webgl");
 
 const sizes = {
-  width:window.innerWidth,
-  height:window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-const camera = new THREE.PerspectiveCamera (
-  75 ,
-  sizes.width / sizes.height ,
-  0.1 ,
+// for a change in the screen size (canvas size responsive)
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); // to avoid blurry screen (every device has different pixel ratio so do this to have a good resolution on all devices)
+});
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
   100
- ) ;
- camera.position.z = 20;
+);
+camera.position.z = 20;
 
- scene.add(camera);
+scene.add(camera);
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
 renderer.setSize(sizes.width, sizes.height);
 
 renderer.render(scene, camera);
-
- 
